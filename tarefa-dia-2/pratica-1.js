2ff80c7fd4f6bc26c4bbbfd699c6aa77e39eada7
André Luiz <andre_aceofbase@yahoo.com.br>
2022-08-24 15:34:42 -0300

    Programando um formulário

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -1,5 +1,28 @@
 console.log("Nome")
+let Nome
+Nome = "André Luiz"
+console.log(Nome)
 console.log("email")
+let email
+email = "andre_aceofbase@yahoo.com.br"
+console.log(email)
 console.log("profissao")
+let profissao
+profissao = "engenheiro"
+console.log(profissao)
 console.log("idade")
+let idade
+idade = "44"
+console.log(idade)
 console.log("Pais")
+let Pais
+Pais = "Portugal"
+console.log(Pais)
+
+
+
+
+
+
+
+
