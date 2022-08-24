70966a6cf6062b8238a6495c3ce31287e88e6bb1
André Luiz <andre_aceofbase@yahoo.com.br>
2022-08-24 16:02:30 -0300

    Programação utilizando template strings

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -19,7 +19,7 @@ let Pais
 Pais = "Portugal"
 console.log(Pais)
 
-
+console.log(`Olá, ${Nome}! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para ${Pais}. Confirma pra gente alguns dados? A sua idade é ${idade}, você é ${profissao} e o seu email é ${email} ? Ficamos no aguardo. Muito obrigado e boa viagem! :D`)
 
 
 
