4583b727b1d360477e8f4ff2ee82414599c2bc31
André Luiz <andre_aceofbase@yahoo.com.br>
2022-08-25 16:36:38 -0300

    pratica 2 do terceiro dia

---
+++ tarefa-dia-3/pratica-2.js
@@ -0,0 +1,13 @@
+
+
+let valordeentrada = 10
+let n = 20
+let contagem = 1
+
+while (contagem <= n) {
+    
+    let resultado = valordeentrada * contagem
+let mostrarcalculo = `${valordeentrada} x ${contagem} = ${resultado}`
+    console.log(mostrarcalculo)
+    contagem = contagem + 1
+}
